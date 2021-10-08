bdf7a530285b4ae24f21c09b9f7db5e6316623e3
Chelsea Troy <chelsea.dommert@gmail.com>
2021-10-08 14:46:09 -0500

    Prepare schema for FEDERATION

--- src/components/get_syndicated_related_content_resolver.py
+++ src/components/get_syndicated_related_content_resolver.py
@@ -1,15 +1,15 @@
 
-def getSyndicatedRelatedContent_resolver(obj, info, syndicated_article_id):
+def getSyndicatedRelatedContent_resolver(obj, info, url):
     # 1. get the syndicated article id
     # 2. Get text from content db using #1
     # 3. vectorize using doc2vec and #2
-    # 4. Get similar articles from vectgor db using #3
+    # 4. Get similar articles from vector db using #3
 
     payload = {
         "message": "Am I working?",
         "relatedItems": [
-            {"title": "100 bottles", "url": "ofbeer.com"},
-            {"title": "99 bottles", "url": "0beer.com"}
+            {"givenUrl": "ofbeer.com"},
+            {"givenUrl": "0beer.com"}
         ]
     }
     return payload
