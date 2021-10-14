f2c08fa4a809b5c906fa58ce634db5a92ab6e21b
Chelsea Troy <chelsea.dommert@gmail.com>
2021-10-14 16:38:06 -0500

    Try out an abstract contract and concrete implementation for vector database result
    + I'm not sure I like this. An abstract contract insinuates the evantual need for multiple concrete implementations, which we aren't likely to have. But there seems to be a lot of concern about whether we might want to choose another vector db in the near future and switch it out, so this change will explicitly guide and limit what would need to change in this API if we were to do that switch

--- src/components/vector_database_client.py
+++ src/components/vector_database_client.py
@@ -1,7 +1,30 @@
 import pinecone
 from dataclasses import dataclass
+from abc import ABC, abstractmethod
 
-class VectorDatabaseClient():
+@dataclass
+class Match:
+    givenUrl: str
+    id: str
+
+@dataclass
+class MatchResult:
+    matches: [Match]
+
+class VectorDatabaseClient(ABC):
+    @abstractmethod
+    def get_similar_vectors(self, key_vector: [float], num_results: int = 5) -> MatchResult:
+        ...
+
+@dataclass
+class PineconeResult:
+    @classmethod
+    def from_dict(cls, dictionary):
+        matches_from_dict = dictionary.get('results')[0].get('matches')
+        matches_as_objects = [Match(id=match.get('id'), givenUrl='getpocket.com') for match in matches_from_dict]
+        return MatchResult(matches=matches_as_objects)
+
+class PineconeDatabaseClient(VectorDatabaseClient):
     def __init__(self, api_key: str, region: str = 'us-west1-gcp'):
         self.api_key = api_key
         self.region = region
@@ -16,25 +39,3 @@ class VectorDatabaseClient():
             include_values=False
         )
         return PineconeResult.from_dict(result.to_dict()) #There's gotta be a more efficient way to serialize this
-
-@dataclass
-class PineconeMatch:
-    id: str
-    score: float
-    values: [float]
-
-@dataclass
-class PineconeData:
-    matches: [PineconeMatch]
-
-@dataclass
-class PineconeResult:
-
-    results: [PineconeData]
-
-    @classmethod
-    def from_dict(cls, dictionary):
-        matches_from_dict = dictionary.get('results')[0].get('matches')
-        matches_as_objects = [PineconeMatch(id=match.get('id'), score=match.get('score'), values=match.get('values')) for match in matches_from_dict]
-        pinecone_data = PineconeData(matches=matches_as_objects)
-        return PineconeResult(results=pinecone_data)
